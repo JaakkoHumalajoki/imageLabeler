e657af7fd50c343cab0b254ca5cc815215bab29b
Jaakko Humalajoki <jaakko.humalajoki@gmail.com>
2022-11-02 12:02:22 +0200

    add Clarifai API call

--- src/App.js
+++ src/App.js
@@ -6,28 +6,34 @@ import Footer from './components/Footer/Footer';
 import Input from './components/Input/Input';
 import ImageDisplay from './components/ImageDisplay/ImageDisplay';
 
-const client = createClient(process.env.REACT_APP_PEXELS_API_KEY);
+const pexelsAPI = process.env.REACT_APP_PEXELS_API_KEY;
+const pexelsClient = createClient(pexelsAPI);
+
+const PAT = process.env.REACT_APP_CLARIFAI_API_KEY;
+const USER_ID = process.env.REACT_APP_CLARIFAI_USER_ID;
+const APP_ID = process.env.REACT_APP_CLARIFAI_APP_ID;
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      background_url: "https://images.pexels.com/photos/14099311/pexels-photo-14099311.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=627&w=1200",
-      input: "https://images.pexels.com/photos/14099311/pexels-photo-14099311.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=627&w=1200",
+      background_url: "",
+      input: "",
       image_url: "",
+      concepts: [],
     };
   }
 
-  // componentDidMount() {
-  //   client.photos.curated({ per_page: 1 })
-  //     .then(photos => {
-  //       const photo_url = photos.photos[0].src.landscape;
-  //       this.setState({ background_url: photo_url });
-  //     })
-  //     .catch(error => {
-  //       console.log(error);
-  //     })
-  // }
+  componentDidMount() {
+    pexelsClient.photos.curated({ per_page: 1 })
+      .then(photos => {
+        const photo_url = photos.photos[0].src.landscape;
+        this.setState({ background_url: photo_url, input: photo_url});
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  }
 
   handleInputChange = (event) => {
     this.setState({ input: event.target.value });
@@ -35,6 +41,42 @@ class App extends Component {
 
   handleButtonClick = () => {
     this.setState({ image_url: this.state.input });
+
+    // Asking Clarifai API for image labels
+
+    const raw = JSON.stringify({
+      "user_app_id": {
+      "user_id": USER_ID,
+      "app_id": APP_ID
+      },
+      "inputs": [
+      {
+          "data": {
+          "image": {
+              "url": this.state.input,
+          }
+          }
+      }
+      ]
+  });
+  
+  const requestOptions = {
+      method: 'POST',
+      headers: {
+      'Accept': 'application/json',
+      'Authorization': 'Key ' + PAT
+      },
+      body: raw
+  };
+
+    fetch("https://api.clarifai.com/v2/models/general-image-recognition/outputs", requestOptions)
+        .then(response => response.json())
+        .then(result => {
+          const concepts = result.outputs[0].data.concepts.map(concept => concept.name);
+          this.setState({ concepts: concepts });
+          console.log(concepts);
+        })
+        .catch(error => console.log('error', error));
   }
 
   render() {
